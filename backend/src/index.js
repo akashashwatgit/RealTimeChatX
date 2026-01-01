0571dd035bc69c774124f411a907014e83f94d98
akashashwatgit <31s.chauhan@gmail.com>
2026-01-01 19:16:35 +0530

    `Added login, logout, updateProfile and checkAuth routes, updated auth controller and user model, added cookie parser middleware`

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,11 +5,12 @@ dotenv.config();
 import authRoutes from "./routes/auth.route.js";
 import { connectDB } from "./lib/db.js";
 const app=express();
+import cookieParser from "cookie-parser";
 
 app.use(express.json()); //allows us to extract user data
-
+app.use(cookieParser());//to parse cookies from request
 app.use("/api/auth",authRoutes);
-const PORT=process.env.PORT || 5001
+const PORT=process.env.PORT
 
 app.listen(PORT,()=>{
     console.log("server is running on",PORT);
--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -10,6 +10,4 @@ export const generateToken=(userID,res)=>{
         sameSite:"strict", //prevents CSRF attacks cross site request forgery attacks
         secure:process.env.NODE_ENV!=="development"
     })
-
-    return token; 
 }

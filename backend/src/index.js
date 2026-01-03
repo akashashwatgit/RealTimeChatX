09ad769f3c2226dedbba7a01215447a045bee322
akashashwatgit <173206486+akashashwatgit@users.noreply.github.com>
2026-01-03 23:23:27 +0530

    Add CORS support, implement authentication checks, and create essential pages for user management

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,7 +7,11 @@ import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
 const app=express();
 import cookieParser from "cookie-parser";
-
+import cors from 'cors';
+app.use(cors({
+    origin:'http://localhost:5173',
+    credentials:true,
+}))
 app.use(express.json()); //allows us to extract user data
 app.use(cookieParser());//to parse cookies from request
 app.use("/api/auth",authRoutes);
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,37 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Navbar from './components/Navbar'
-import { Routes ,Route} from 'react-router-dom'
+import { Routes ,Route, Navigate} from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import SignUpPage from './pages/SignUpPage'
+import LoginPage from './pages/LoginPage'
+import ProfilePage from './pages/ProfilePage'
+import SettingsPage from './pages/SettingsPage'
+import { useAuthStore } from './store/useAuthStore'
+import {Loader} from 'lucide-react';
+
 
 const App = () => {
+
+  const {authUser,checkAuth,isCheckingAuth}=useAuthStore();
+  useEffect(()=>{
+    checkAuth()
+  },[checkAuth])
+
+  console.log({authUser});
+
+  if(isCheckingAuth && !authUser) return(
+    <div className='flex items-center justify-center h-screen'>
+      <Loader className='size-10 animate-spin'/>
+    </div>
+  )
   return (
    <div>
     <Navbar/>
     <Routes>
-      <Route path='/' element={<HomePage/>}/>
-      <Route path='/signup' element={<SignUpPage/>}/>
-      <Route path='/login' element={<LoginPage/>}/>
-      <Route path='/profile' element={<ProfilePage/>}/>
+      <Route path='/' element={authUser ?<HomePage/>: <Navigate to = "/login"/>}/>
+      <Route path='/signup' element={!authUser?<SignUpPage/>:<Navigate to= "/"/>}/>
+      <Route path='/login' element={!authUser?<LoginPage/>:<Navigate to ="/"/>}/>
+      <Route path='/profile' element={authUser ? <ProfilePage/>: <Navigate to ="/login"/>}/>
       <Route path='/settings' element={<SettingsPage/>}/>
     </Routes>
    </div>
